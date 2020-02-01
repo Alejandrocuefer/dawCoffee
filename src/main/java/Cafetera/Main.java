8fd6eeac33c9930a5127a1127bdb986781101607
Gabriel <gabionezero@gmail.com>
2020-02-01 22:42:00 +0100

    Prueba la creación de los objetos en el Main

--- src/main/java/Cafetera/Main.java
+++ src/main/java/Cafetera/Main.java
@@ -15,8 +15,20 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-      
+
+        Deposito depositoCafe = new Deposito(1000, 200, 500, EnumDepositos.CAFE);
+        Deposito depositoAgua = new Deposito(1000, 200, 500, EnumDepositos.AGUA);
+        Deposito depositoLeche = new Deposito(1000, 200, 500, EnumDepositos.LECHE);
+        Deposito depositoChoco = new Deposito(1000, 200, 500, EnumDepositos.CHOCOLATE);
+        Deposito depositoAzucar = new Deposito(1000, 200, 500, EnumDepositos.AZUCAR);
+
+        Usuario admin = new Usuario("Pepe", "1234");
+
+        Monedero monedero = new Monedero();
+
+        Cafetera cafetera = new Cafetera(depositoCafe, depositoAgua, depositoLeche, depositoChoco, depositoAzucar, admin, monedero);
         
+        cafetera.inicializar();
     }
-    
+
 }

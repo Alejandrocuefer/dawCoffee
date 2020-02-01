c1552baa1ada612eba435d883d138444463765d7
Gabriel <gabionezero@gmail.com>
2020-02-01 22:44:05 +0100

    Crea constructor para la clase Usuario

--- src/main/java/Cafetera/Usuario.java
+++ src/main/java/Cafetera/Usuario.java
@@ -13,5 +13,12 @@ public class Usuario {
     
     private String userName;
     private String password;
+
+    public Usuario(String userName, String password) {
+        this.userName = userName;
+        this.password = password;
+    }
+    
+    
     
 }
